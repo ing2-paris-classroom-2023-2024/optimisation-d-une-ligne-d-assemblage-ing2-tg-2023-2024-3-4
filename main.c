ea8d91d37eec67166ba578ede5b350d5b323289c
marinasna <marina.sa.nascimento@gmail.com>
2023-11-22 08:38:21 +0100

    creation structure tache, lecture ordre (nb tache) et temps

--- main.c
+++ main.c
@@ -1,17 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void lire_graphe(char * nomFichier) {
+typedef struct tache
+{
+    float temps;
+    int num;
+    int* tab_exclu;
+}taches;
 
-    // ouverture fichier manuel
-    FILE *ifs = fopen(nomFichier, "r");
-    int op1, op2;
 
-    if (!ifs) {
-        printf("Erreur de lecture fichier\n");
-        exit(-1);
+// pb comment trouver ordre sans lire données
+int trouver_ordre(FILE* op)
+{
+    int nb_tache = 0;
+
+    int poubelle1;
+    float poubelle2;
+
+    // s'arrete quand le fichier est vide
+    while (feof(op)==0)
+    {
+        fscanf(op, "%d%f", &poubelle1, &poubelle2);
+        if (feof(op)!=0) break;
+
+        // compte taille du fichier
+        nb_tache++;
     }
+    rewind(op);
 
+    return nb_tache;
+}
+
+void init(FILE* op, taches* t, int ordre)
+{
+    for (int i = 0; i < ordre; ++i) {
+        fscanf(op, "%d %f", &t[i].num, &t[i].temps);
+    }
+}
+
+void lire_graphe(FILE* ifs) {
+
+    int op1, op2;
 
     // faire des tableau dynamique
     int tab[100];
@@ -33,11 +62,12 @@ void lire_graphe(char * nomFichier) {
         i++;
         j++;
 
+
         // compte taille du fichier (nombre d'exclusions)
         compteur++;
-    }
 
-    for (int k = 0; k < compteur; ++k) {
+    }
+        for (int k = 0; k < compteur; ++k) {
         printf("%d %d\n", tab[k], tabj[k]);
     }
 
@@ -45,20 +75,35 @@ void lire_graphe(char * nomFichier) {
 
 int main() {
 
-    FILE * fp;
-    fp= fopen("../exclusions.txt", "r");
-    if (fp == NULL) {
-        perror("Erreur lors de l'ouverture du fichier");
-        return 1;
+    FILE *exclu = fopen("../exclusions.txt", "r");
+    if (exclu == NULL) {
+        printf("Erreur de lecture fichier\n");
+        exit(-1);
     }
 
-    int* tab;
+    FILE *op = fopen("../operations.txt", "r");
+    if (!op) {
+        printf("Erreur de lecture fichier\n");
+        exit(-1);
+    }
 
-    char nom_fichier[50];
-    printf("entrer le nom du fichier du labyrinthe (format:../nom_du_fichier):\n");
-    gets(nom_fichier);
 
-    lire_graphe(nom_fichier);
+
+    int ordre = trouver_ordre(op);
+    printf("%d\n", ordre);
+
+
+    taches* tache;
+    tache = (taches*)malloc(ordre*sizeof(taches));
+
+
+    init(op, tache, ordre);
+    for (int i = 0; i < ordre; ++i) {
+        printf("%d de %f\n", tache[i].num, tache[i].temps);
+    }
+
+
+    lire_graphe(exclu);
 
 
     return 0;
